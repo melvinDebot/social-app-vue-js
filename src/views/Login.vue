286009e44086ff4813c26aa46222d234c2f87075
melvinDebot <melvindebot@gmail.com>
2020-08-20 19:11:59 +0200

    add style login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,18 +4,14 @@
     <section>
       <div class="col1">
         <h1>Vuegram</h1>
-        <p>Welcome to the <a href="https://savvyapps.com/" target="_blank">Savvy Apps</a> sample social media web app powered by Vue.js and Firebase.
-          Build this project by checking out The Definitive Guide to Getting Started with Vue.js</p>
       </div>
       <div :class="{ 'signup-form': !showLoginForm }" class="col2">
         <form v-if="showLoginForm" @submit.prevent>
           <h1>Welcome Back</h1>
           <div>
-            <label for="email1">Email</label>
-            <input v-model.trim="loginForm.email" type="text" placeholder="you@email.com" id="email1" />
+            <input v-model.trim="loginForm.email" type="text" placeholder="mail" id="email1" />
           </div>
           <div>
-            <label for="password1">Password</label>
             <input v-model.trim="loginForm.password" type="password" placeholder="******" id="password1" />
           </div>
           <button @click="login()" class="button">Log In</button>
@@ -27,20 +23,16 @@
         <form v-else @submit.prevent>
           <h1>Get Started</h1>
           <div>
-            <label for="name">Name</label>
-            <input v-model.trim="signupForm.name" type="text" placeholder="Savvy Apps" id="name" />
+            <input v-model.trim="signupForm.name" type="text" placeholder="Username" id="name" />
           </div>
           <div>
-            <label for="title">Title</label>
-            <input v-model.trim="signupForm.title" type="text" placeholder="Company" id="title" />
+            <input v-model.trim="signupForm.title" type="text" placeholder="prénom" id="title" />
           </div>
           <div>
-            <label for="email2">Email</label>
             <input v-model.trim="signupForm.email" type="text" placeholder="you@email.com" id="email2" />
           </div>
           <div>
-            <label for="password2">Password</label>
-            <input v-model.trim="signupForm.password" type="password" placeholder="min 6 characters" id="password2" />
+            <input v-model.trim="signupForm.password" type="password" placeholder="password min 6 characters" id="password2" />
           </div>
           <button @click="signup()" class="button">Sign Up</button>
           <div class="extras">
